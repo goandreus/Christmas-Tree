3ee82bc0ede3c8d2a05a05dc6de6e43274dfb8dc
goandreus <andres.chavez.crisostomo@gmail.com>
2020-12-02 23:20:48 -0500

    format tree (#3)

--- lib/ui/tree_page.dart
+++ lib/ui/tree_page.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:TreeChristmas/widgets/light.dart';
 import 'package:flutter/material.dart';
 
 class TreePage extends StatelessWidget {
@@ -55,56 +56,3 @@ class TreePage extends StatelessWidget {
   }
 }
 
-class Light extends StatefulWidget {
-  Light({Key key, this.x})
-      : period = 500 + (x.abs() * 5000).floor(),
-        color = festiveColors[Random().nextInt(4)],
-        super(key: key);
-
-  static final festiveColors = [
-    Colors.red,
-    Colors.green,
-    Colors.yellow,
-    Colors.orange
-  ];
-
-  final double x;
-
-  final int period;
-
-  final Color color;
-
-  @override
-  _LightState createState() => _LightState();
-}
-
-class _LightState extends State<Light> {
-  Color _newColor = Colors.white;
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 10,
-      child: Align(
-        alignment: Alignment(widget.x, 0.0),
-        child: AspectRatio(
-          aspectRatio: 1,
-          child: TweenAnimationBuilder(
-            duration: Duration(milliseconds: widget.period),
-            tween: ColorTween(begin: widget.color, end: _newColor),
-            onEnd: () {
-              setState(() {
-                _newColor =
-                    _newColor == Colors.white ? widget.color : Colors.white;
-              });
-            },
-            builder: (_, color, __) {
-              return Container(
-                color: color,
-              );
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
